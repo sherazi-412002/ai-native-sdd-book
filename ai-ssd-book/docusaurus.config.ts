fa0f609f2d071e5461dbed7bb1875fb59c0e3c20
Syed Shoaib Sherazi <syedshoaibsherazi412002@gmail.com>
2025-12-10 22:04:32 +0500

    updated tsconfig

--- ai-ssd-book/docusaurus.config.ts
+++ ai-ssd-book/docusaurus.config.ts
@@ -125,7 +125,7 @@ const config: Config = {
           items: [
             {
               label: 'Tutorial',
-              to: '/docs/intro',
+              to: '/docs/introduction',
             },
           ],
         },
